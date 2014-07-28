a0606222ba0e8604dbed3ca8f02ab4719b9e5df4
pcchou <philipchou1009@gmail.com>
2014-07-28 18:30:12 +0800

    The first commit.

---
+++ ba4b.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        ba4bscript
+// @namespace   ba4b
+// @description  Better Avatar for Bahamut | 給巴哈姆特一個好看一點的頭像
+// @include     *.gamer.com.tw/*
+// @version     1
+// @grant       none
+// ==/UserScript==
